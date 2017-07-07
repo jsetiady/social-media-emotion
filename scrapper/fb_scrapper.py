4872d7534e6c237662f95bd7fdfbd79b35f7578f
Jessie Setiady <jessie.s@aux.go-jek.com>
2017-07-07 11:04:34 +0700

    set the request limit to 100

--- scrapper/fb_scrapper.py
+++ scrapper/fb_scrapper.py
@@ -6,33 +6,48 @@ import csv
 import sys
 
 # You might have to update the token
-token= ‘_YOUR_ACCESS_TOKEN’
+token= '_YOUR_ACCESS_TOKEN_'
 gojek_id = "166677693366218"
 base_url = "https://graph.facebook.com/v2.7/"
 graph = facebook.GraphAPI(access_token=token, version = 2.7)
 
 str_comments = []
 
+posts_next_page = base_url + gojek_id + "/posts?limit=100&access_token="+token
 
-# Get post id
-posts = requests.get(base_url + gojek_id + "/posts?limit=100&access_token="+token) 
-posts_json = posts.json()
+while posts_next_page != "":
+	posts = requests.get(posts_next_page) 
+	posts_json = posts.json()
+	print "posts"
 
-print posts_json
-sys.exit()
+    # Get post id
+	for x in range(0, len(posts_json['data'])):
+		post_id = posts_json['data'][x]['id']
 
-for x in range(0, len(posts_json['data'])):
-	post_id = posts_json['data'][x]['id']
-	comments = requests.get(base_url + post_id + "/comments?limit=100&access_token="+token) 
-	comments_json = comments.json()
-	for y in range(0, len(comments_json['data'])):
-		str_comments.append(comments_json['data'][y]['message'].encode('utf-8'))
+		comments_next_page = base_url + post_id + "/comments?limit=100&access_token="+token
+		comments = requests.get(comments_next_page)
+		comments_json = comments.json()
+		print "comments " + str(x)
+		if comments_json.has_key('data'):
+			for y in range(0, len(comments_json['data'])):
+				str_comments.append(comments_json['data'][y]['message'].encode('utf-8'))
 
-feed = requests.get(base_url + gojek_id + "/feed?limit=100&access_token="+token) 
-feeds_json = feed.json()
+	if posts_json['paging'] != "" and posts_json['paging']['next'] != "":
+		posts_next_page = posts_json['paging']['next']
+		posts_next_page = base_url + posts_next_page[32:]
+	else:
+		posts_next_page = ""
+
+#feed
+feeds_next_page = base_url + gojek_id + "/feed?limit=100&access_token="+token
+feeds = requests.get(feeds_next_page)
+feeds_json = feeds.json()
 
 for x in range(0, len(feeds_json['data'])):
-	str_comments.append(feeds_json['data'][x]['message'].encode('utf-8'))
+	print "feeds " + str(x)
+	if feeds_json['data'][x].has_key('message'):
+		str_comments.append(feeds_json['data'][x]['message'].encode('utf-8'))
+
 
 # Write to CSV
 f = open("test_fb.csv", "wb+")
