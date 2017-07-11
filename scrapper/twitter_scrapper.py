a87c8f6140395e909461a087055302be6ff8bce4
Jessie Setiady <jessie.s@aux.go-jek.com>
2017-07-11 10:25:35 +0700

    code refactoring

--- scrapper/twitter_scrapper.py
+++ scrapper/twitter_scrapper.py
@@ -1,91 +1,38 @@
-import re
-import tweepy 
-import json
+import re, tweepy, json, csv, sys, time
 from tweepy import OAuthHandler
  
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_secret = ''
- 
+consumer_key = '_YOUR_CONSUMER_KEY_'
+consumer_secret = '_YOUR_CONSUMER_SECRET_'
+access_token = '_YOUR_ACCESS_TOKEN_'
+access_secret = '_YOUR_ACCESS_SECRET_'
+
+csv_filename = "twitter.csv"
+
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
  
 api = tweepy.API(auth)
 
-print "["
-
-def process_or_store(tweet):
-    tweet = json.dumps(tweet)
-    tweet = re.sub(r"\\u(.){4}", " ", tweet)
-    print tweet
-    print ", "
-
-#for tweet in tweepy.Cursor(api.user_timeline).items():
-#    process_or_store(tweet._json)
-'''
-for tweet in tweepy.Cursor(api.search, q="gojek OR go-jek \
-	OR gopay OR go-pay OR goclean OR go-clean \
-	gofood OR go-food OR goride OR go-ride \
-	gocar OR go-car OR gosend OR go-send").items():
-    process_or_store(tweet._json)
-'''
+def limit_handled(cursor):
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.TweepError:
+            print "sleep"
+            time.sleep(15 * 60)
 
 
-for tweet in tweepy.Cursor(api.search, q="gojek OR go-jek").items():
-    process_or_store(tweet._json)
 
-print "]"
-'''
-for tweet in tweepy.Cursor(api.search, q="gopay OR go-pay").items():
-    process_or_store(tweet._json)
+f = open(csv_filename, "wb+")
+writer = csv.writer(f)
 
-for tweet in tweepy.Cursor(api.search, q="goclean OR go-clean").items():
-    process_or_store(tweet._json)
+for tweet in limit_handled(tweepy.Cursor(api.search, q="gojek OR go-jek OR @gojekindonesia").items()):
+    t = tweet._json
+    t = json.dumps(t["text"])
+    t = re.sub(r"\\u(.){4}", " ", t) # remove unicode character
+    t = re.sub(r"http(.)*?(\s|$)", "", t) # remove url / link
+    t = re.sub(r"\s\s+", " ", t) # remove multi-space
+    print t
+    # Write to CSV
+    writer.writerow([t])
 
-for tweet in tweepy.Cursor(api.search, q="gofood OR go-food").items():
-    process_or_store(tweet._json)
-
-for tweet in tweepy.Cursor(api.search, q="goride OR go-ride").items():
-    process_or_store(tweet._json)
-'''
-'''
-for tweet in tweepy.Cursor(api.search, q="gocar OR go-car").items():
-    process_or_store(tweet._json)
-
-for tweet in tweepy.Cursor(api.search, q="gosend OR go-send").items():
-    process_or_store(tweet._json)
-
-for tweet in tweepy.Cursor(api.search, q="gopulsa OR go-pulsa").items():
-    process_or_store(tweet._json)
-
-for tweet in tweepy.Cursor(api.search, q="goshop OR go-shop").items():
-    process_or_store(tweet._json)
-
-for tweet in tweepy.Cursor(api.search, q="gomart OR go-mart").items():
-    process_or_store(tweet._json)    
-
-for tweet in tweepy.Cursor(api.search, q="gotix OR go-tix").items():
-    process_or_store(tweet._json)
-
-for tweet in tweepy.Cursor(api.search, q="gobox OR go-box").items():
-    process_or_store(tweet._json)
-
-for tweet in tweepy.Cursor(api.search, q="gomassage OR go-massage").items():
-    process_or_store(tweet._json)
-
-for tweet in tweepy.Cursor(api.search, q="goglam OR go-glam").items():
-    process_or_store(tweet._json)
-
-for tweet in tweepy.Cursor(api.search, q="goclean OR go-clean").items():
-    process_or_store(tweet._json)
-
-for tweet in tweepy.Cursor(api.search, q="goauto OR go-auto").items():
-    process_or_store(tweet._json)
-
-for tweet in tweepy.Cursor(api.search, q="gomed OR go-med").items():
-    process_or_store(tweet._json)
-
-for tweet in tweepy.Cursor(api.search, q="gobusway OR go-busway").items():
-    process_or_store(tweet._json)
-'''
