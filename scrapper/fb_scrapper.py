e65a3fb384528a560eb6fa94643c109acadad325
Jessie Setiady <jessie.s@aux.go-jek.com>
2017-07-11 10:27:00 +0700

    code refactoring

--- scrapper/fb_scrapper.py
+++ scrapper/fb_scrapper.py
@@ -5,9 +5,13 @@ token= '_YOUR_ACCESS_TOKEN_'
 gojek_id = "166677693366218"
 base_url = "https://graph.facebook.com/v2.7/"
 graph = facebook.GraphAPI(access_token=token, version = 2.7)
+filename = "facebook.csv"
+
+f = open(filename, "wb+")
+writer = csv.writer(f)
+
 
 comment_message = []
-
 posts_next_page = base_url + gojek_id + "/posts?limit=100&access_token="+token
 
 k = 0
@@ -24,14 +28,19 @@ while posts_next_page != "" and k != 10:
 		while comments_next_page != "":
 			comments = requests.get(comments_next_page)
 			comments_json = comments.json()
+
 			print "comments " + str(x)
 			if comments_json.has_key('data'):
 				print "Total comment: " + str(len(comments_json['data']))
+
 				for y in range(0, len(comments_json['data'])):
-					comment_message.append([ \
+					data = [ \
 						comments_json['data'][y]['message'].encode('utf-8'),
 						comments_json['data'][y]['created_time'].encode('utf-8'),
-						comments_json['data'][y]['id'].encode('utf-8')])
+						comments_json['data'][y]['id'].encode('utf-8')]
+					comment_message.append(data)
+					writer.writerow(item)
+
 				if comments_json.has_key('paging') and comments_json['paging'].has_key('next'):
 					comments_next_page = comments_json['paging']['next']
 					comments_next_page = base_url + comments_next_page[32:]
@@ -51,22 +60,18 @@ while feeds_next_page != "":
 	feeds_json = feeds.json()
 
 	for x in range(0, len(feeds_json['data'])):
+
 		print "feeds " + str(x)
 		if feeds_json['data'][x].has_key('message'):
-			comment_message.append([ \
+			data = [ \
 				feeds_json['data'][x]['message'].encode('utf-8'), \
 				feeds_json['data'][x]['created_time'].encode('utf-8'), \
-				feeds_json['data'][x]['id'].encode('utf-8')])
+				feeds_json['data'][x]['id'].encode('utf-8')]
+			comment_message.append(data)
+			writer.writerow(item)
 
 	if feeds_json.has_key('paging') and feeds_json['paging'].has_key('next'):
 		feeds_next_page = feeds_json['paging']['next']
 		feeds_next_page = base_url + feeds_next_page[32:]
 	else:
 		feeds_next_page = ""
-
-# Write to CSV
-f = open("test.csv", "wb+")
-print "write to csv"
-writer = csv.writer(f)
-for item in comment_message:
-    writer.writerow(item)
